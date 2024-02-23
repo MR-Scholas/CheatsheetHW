b9d68fb39bc0d9ffd1e504c42b4307822d3d148e
MR-Scholas <160638944+MR-Scholas@users.noreply.github.com>
2024-02-23 06:01:17 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+//ketyword --- name --- () => {}
+const changeButton = () => 
+{
+    //this is where i put my instructions
+    let ourButton = document.querySelectorAll{'.attnBtn'}
+
+    ourButton.innerHTML = "COMPLETED"
+}
